3275def660f7945989a2343d71a11188d8c0753c
Ignacio <85964269+ignaciorosso@users.noreply.github.com>
2021-11-02 18:43:23 -0300

    Add files via upload

--- Prueba 1/Prueba1v2.py
+++ Prueba 1/Prueba1v2.py
@@ -1,12 +1,15 @@
+# Tomamos un fragmento de texto de un archivo con .txt
 
+with open(r'C:\Users\Ignacio\Desktop\Prueba Tecnica\text.txt', 'r') as f:
+    text = f.read()
 
-text = "Supongamos un texto bien escrito, donde no tengamos tanto lio. ¿Que pasaria? Seguramente, ¡Nada!"
+# Procesamos el texto para dejarlo lo mas simple posible, sin simbolos ni mayusculas   
 
 def texto_minuscula(text):
     return text.lower()
 
 def reemplazar_simbolos(text):
-    lista = [",", ".", '!', '?', '¿', '¡']
+    lista = [',', '.', '!', '?', '¿', '¡', ':', '(', ')']
     new_text = text
     for simb in lista:
         new_text = new_text.replace(simb, '')
@@ -15,10 +18,33 @@ def reemplazar_simbolos(text):
 def convertir_lista(text):
     return text.split()
 
+# Cargamos las palabras en un diccionario, de esta manera podemos visualizar de un mejor modo
+# la cantidad de veces que se repiten algunas palabras
+
+def crear_diccionario(lista):
+    diccionario = {}
+    for palabra in lista: 
+        diccionario[palabra] = lista.count(palabra)
+    return diccionario
+
+def mas_repetida(diccionario):
+    mayor = 0
+    for palabra in diccionario:
+        if (diccionario[palabra] > mayor):
+            mayor = diccionario[palabra]
+            rep_palabra = palabra
+    return f'Palabra que mas se repite: "{rep_palabra}", {mayor} veces.'
+
+# Bloque principal del programa 
+
 texto1 = texto_minuscula(text)
 texto2 = reemplazar_simbolos(texto1)
 lista = convertir_lista(texto2)
+diccionario = crear_diccionario(lista)
+
 
 print(f'Pasamos el texto a minusculas: \n{texto1}')
 print(f'Reemplazamos los simbolos: \n{texto2}')
 print(f'Convertimos a lista: \n{lista}')
+print(diccionario)
+print(mas_repetida(diccionario))
